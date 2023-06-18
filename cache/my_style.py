4d620ed5990d70be16ad097d50fc1b85a3f504f7
Bartolomeo <b.fiorini@qmul.ac.uk>
2023-06-18 19:35:47 +0100

    Add emulator's cached data

---
+++ cache/my_style.py
@@ -0,0 +1,51 @@
+axes.grid: 1
+axes.labelsize: 14.0
+axes.linewidth: 1.5
+axes.prop_cycle: cycler('color', ['1f77b4', 'ff7f0e', '2ca02c', 'd62728', '9467bd', '8c564b', 'e377c2', '7f7f7f', 'bcbd22', '17becf'])
+axes.titlesize: 18.0
+    
+figure.edgecolor: 'white'
+figure.facecolor: 'white'
+figure.figsize: 8.08, 5.0
+    
+font.size: 14.0
+font.family: sans-serif
+
+
+legend.fancybox: True
+legend.fontsize: 16.0
+    
+lines.dash_capstyle: butt
+lines.linewidth: 1.35
+lines.markeredgewidth: 1.0
+lines.markersize: 4.0
+lines.dashed_pattern : 7, 7
+lines.dashdot_pattern : 3, 5, 1, 5
+lines.dotted_pattern : 1, 3
+lines.scale_dashes: True
+    
+savefig.bbox: tight
+savefig.format: pdf
+savefig.transparent: False
+text.usetex: False
+xtick.labelsize: 14.0
+xtick.major.size: 6.0
+xtick.major.width: 1.5
+xtick.minor.size: 4.0
+xtick.minor.width: 1.
+ytick.labelsize: 14.0
+ytick.major.size: 6.0
+ytick.major.width: 1.5
+ytick.minor.size: 4.0
+ytick.minor.width: 1.
+xtick.top:True
+ytick.right:True
+xtick.direction:in
+ytick.direction:in
+
+### GRIDS
+grid.color       :   k       # grid color
+grid.linestyle   :   --       # dashed
+grid.linewidth   :   0.5     # in points
+grid.alpha       :   0.5     # transparency, between 0.0 and 1.0
+          
